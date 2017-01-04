95a9f9fbca71ff36651711e30203561bd5071595
linghualuoxue <y3991721l>
2017-01-04 11:49:01 +0800

    项目类

---
+++ LogDataReceive/src/main/java/com/bj/sxt/LogDataReceive.java
@@ -0,0 +1,15 @@
+package com.bj.sxt;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by user on 2017/1/4.
+ */
+public class LogDataReceive {
+    private static final Logger log = Logger.getGlobal();
+    private static final String PLAT_FORM = "java-server";
+    private static final String SDK = "java";
+    private static final String VERSION = "1";
+
+
+}
