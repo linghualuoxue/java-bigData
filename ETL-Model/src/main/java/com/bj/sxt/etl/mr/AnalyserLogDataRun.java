dc23ee5c81aa40b5d590624f32b963d7bdf001c2
linghualuoxue <y3991721l>
2017-01-13 17:24:55 +0800

    r

--- ETL-Model/src/main/java/com/bj/sxt/etl/mr/AnalyserLogDataRun.java
+++ ETL-Model/src/main/java/com/bj/sxt/etl/mr/AnalyserLogDataRun.java
@@ -1,6 +1,12 @@
 package com.bj.sxt.etl.mr;
 
+import com.bj.sxt.common.EventLogConstants;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
@@ -22,17 +28,29 @@ public class AnalyserLogDataRun implements Tool{
         }
     }
     @Override
-    public int run(String[] strings) throws Exception {
+    public int run(String[] args) throws Exception {
+        Configuration conf =this.getConf();
+        this.processArgs(conf,args);
+        Job job = Job.getInstance(conf,"analyser_data");
+        job.setJarByClass(AnalyserLogDataRun.class);
+        job.setMapperClass(AnalyserLogDataMapper.class);
+        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(Put.class);
+        TableMapReduceUtil.initTableReducerJob(EventLogConstants.HBASE_NAME_EVENT_LOGS,null,job,null,null,null,null,false);
+
         return 0;
     }
 
     @Override
-    public void setConf(Configuration configuration) {
-
+    public void setConf(Configuration conf) {
+        conf.set("fs.defaultFS","http://64centos1:9000");
+        conf.set("yarn.resourcemanager.hostname","64centos1");
+        conf.set("hbase.zookeeper.quorum","64centos1,64centos2,64centos3");
+        this.conf = HBaseConfiguration.create(conf);
     }
 
     @Override
     public Configuration getConf() {
-        return null;
+        return this.conf;
     }
 }
