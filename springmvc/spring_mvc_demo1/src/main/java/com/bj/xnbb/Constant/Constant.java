a6e0f224fbc716cb89323fe1fd5243289303afb4
ylhi@qq.com <y3991721l>
2017-07-20 18:07:51 +0800

    代码修改

---
+++ springmvc/spring_mvc_demo1/src/main/java/com/bj/xnbb/Constant/Constant.java
@@ -0,0 +1,22 @@
+package com.bj.xnbb.Constant;
+
+/**
+ * Created by hasee on 2017/7/20.
+ */
+public class Constant {
+
+    /**
+     * 连接zk的超时时间
+     */
+    public static final int ZK_SESSION_TIMEOUT = 5000;
+
+    /**
+     * 连接zk的注册节点
+     */
+    public static final String ZK_REGIST_PATH = "/web_server";
+
+
+    public static final String ZK_REGIST_DATA = ZK_REGIST_PATH+"/data";
+
+
+}
